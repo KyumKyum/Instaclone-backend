65357832f4fdee11caa6eb4f08f4b4120e4aa445
Jay <mycheesepasta@gmail.com>
2022-08-28 18:10:30 +0900

    dotenv

--- server.js
+++ server.js
@@ -4,13 +4,19 @@
 /*
 Basic code to run Apollo graphql server.
 */
+//import dotenv from "dotenv"
+//dotenv.config(); it won't work
+require('dotenv').config()
+//It don'y have to be re-imported from other files
+
 //const {ApolloServer, gql} = require("apollo-server")
 import {ApolloServer} from "apollo-server";
 import schema from "./schema";
 
 //create server 
 const server= new ApolloServer({schema});
+const PORT = process.env.PORT;
 
 server
-    .listen()
-    .then(() => console.log("Server in running on http://localhost:4000/"));
+    .listen(PORT)
+    .then(() => console.log(`😁 Server in running on http://localhost:${PORT}/`));
