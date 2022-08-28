b1195c079e456cc9d58471d82a0aef8d7d12ad2d
Jay <mycheesepasta@gmail.com>
2022-08-28 15:52:40 +0900

    Practicing CRUD with Prisma

--- server.js
+++ server.js
@@ -6,25 +6,32 @@ Basic code to run Apollo graphql server.
 */
 //const {ApolloServer, gql} = require("apollo-server")
 import {ApolloServer, gql} from "apollo-server";
-//
+import {PrismaClient} from "@prisma/client";
+
+
+const client = new PrismaClient();
 
 //graphql type definitions
 //Defines what kind of field the client going to query about.
 const typeDefs = gql`
     type Movie{
-        id: Int
-        title: String
-        year: Int
+        id: Int!
+        title: String!
+        year: Int!
+        genre: String
+        createdAt: String!
+        updatedAt: String!
     }
     
     type Query {
         movies: [Movie]
-        movie: Movie
+        movie(id: Int!): Movie
     }
 
     type Mutation{
-        createMovie(title:String!): Boolean
-        deleteMovie(title:String!): Boolean
+        createMovie(title:String!, year: Int!, genre: String): Movie
+        deleteMovie(id: Int!): Movie
+        updateMovie(id: Int!, title: String!, year: Int!, genre:String): Movie
     }
 `;
 
@@ -32,19 +39,31 @@ const typeDefs = gql`
 //Defines what results the server return for corresponding queries.
 const resolvers = {
     Query: {
-        movies: () => [],
-        movie: () => ({"title": "Hello", year:2022})
+        movies: () => client.movie.findMany(),
+        movie: (_, {id}) => client.movie.findUnique({
+            where:{ id } //Find unique value which id is same as given args
+        })
     },
     Mutation: {
         //_ -> root (bc we don't need it, ignores it)
-        createMovie: (_, {title}) => {
-            console.log(title); 
-            return true;
-        },
-        deleteMovie: (_,{title}) => {
-            console.log(title);
-            return true;
-        }
+        createMovie: (_, {title, year, genre}) => client.movie.create({
+            data:{ //Client already knows the field information.
+                title, //title:title
+                year,
+                genre,
+            }
+        }),
+        deleteMovie: (_,{id}) => client.movie.delete({
+            where: {id},
+        }),
+        updateMovie: (_,{id, title, year, genre}) => client.movie.update({
+            where:{id}, //Target going to change
+            data:{//Field to change
+                title, 
+                year, 
+                genre
+            }
+        })
     }
 };
 
