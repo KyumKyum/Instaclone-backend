06bb545c61136f1160c5ac4a374a0d5ab0d69d47
Jay <mycheesepasta@gmail.com>
2022-08-29 00:57:07 +0900

    editProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,40 @@
-import { gql } from "apollo-server"
+import bcrypt from "bcrypt"
+import client from "../../client"
 
 export default {
     Mutation: {
-        editProfile: () => console.log("TODO")
+        editProfile: async (_,{
+            firstName,
+            lastName,
+            userName,
+            email,
+            password:pwd,
+        }
+        ) => {
+            let hashedPassword = null;
+            if(pwd){
+                hashedPassword = await bcrypt.hash(pwd, 10);
+            }
+            const updatedUser = await client.User.update({
+                where: {
+                    id:1,
+                }, //TODO
+                data:{ //Prisma doesn't sent data UNDEFINED to DB.
+                    firstName,
+                    lastName,
+                    userName,
+                    email,
+                    ...(hashedPassword && {password:hashedPassword}) // ...(condition &&{Return this object if the condition is true}) 
+                }
+            });
+            if (updatedUser.id) {
+                return {ok: true};
+            }else {
+                return {
+                    ok: false,
+                    error: "Cannot Update User Data"
+                };
+            }
+        }
     }
 }
